803bbdde660cfa8eb014ba85e41cbeee63c4f660
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2024-12-01 00:46:50 +0600

    loop half done

---
+++ Loops/for_of.js
@@ -0,0 +1,15 @@
+// How To Works with For of loops:
+
+const nums = [1, 2, 3, 4, 5];
+
+for (const num of nums) {
+    console.log(num);
+}
+
+/* Output: 
+        1
+        2
+        3
+        4
+        5 
+*/
---
+++ Loops/while_loop.js
@@ -0,0 +1,9 @@
+// How to use While loops:
+
+let num = 0;
+while (num < 5) {
+    console.log('Loop', num);
+    num = num + 1;
+    // num += 1;
+    // num++;
+}
